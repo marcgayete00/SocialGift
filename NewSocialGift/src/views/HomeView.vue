1b2073b24aec9de762587d2a33d4ed55200af042
marcgayete00 <marc.gayete@students.salle.url.edu>
2023-03-13 18:01:31 +0100

    new

--- NewSocialGift/src/views/HomeView.vue
+++ NewSocialGift/src/views/HomeView.vue
@@ -1,31 +1,47 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
 <template>
-  <section id="LanguageSection">
-    <nav>
-      <ol>
-        <li> Idioma </li>
-        <li><select name="select" id="LanguageSelect">
-          <option value="value1">Spanish</option>
-          <option value="value2" selected>I</option>
-          <option value="value3">Value 3</option>
-        </select> </li>
-      </ol>
-    </nav>
+  <body>
+    <section id="LanguageSection">
+      <nav>
+        <ol>
+          <li>Idioma</li>
+          <li>
+            <select name="select" id="LanguageSelect">
+              <option value="value1">Spanish</option>
+              <option value="value2" selected>I</option>
+              <option value="value3">Value 3</option>
+            </select>
+          </li>
+        </ol>
+      </nav>
+    </section>
+
     <section id="GeneralSection">
       <section id="LoginSection">
         <div>
-          <img src="../../src/img/logo.png" id="LogoStyle">
+          <img src="img/logo.png" id="LogoStyle" />
           <form>
-            <input type="text" class="input" name="username" placeholder="Usuario"><br>
-            <input type="password" class="input" id="password" name="password" placeholder="Contraseña"><br>
+            <input type="text" class="input" name="username" placeholder="Usuario" /><br />
+            <input
+              type="password"
+              class="input"
+              id="password"
+              name="password"
+              placeholder="Contraseña"
+            /><br />
             <button type="submit" id="loginButton">Login</button>
-            <hr>
-            <span id="textRegister">¿No tienes cuenta? <a href="#" >Registrate</a></span>
+            <hr />
+            <span id="textRegister">¿No tienes cuenta? <a href="#">Registrate</a></span>
           </form>
         </div>
       </section>
-        <section id="ImageSection">
-          <img src="../../src/img/mainScrenshoot.png">
-        </section>
+
+      <section id="ImageSection">
+        <img src="img/mainScrenshoot.png" />
+      </section>
     </section>
-  </section>
+  </body>
 </template>
