028f83993ef69c2243d4558fc3da7a4e27cb96f3
marcgayete00 <marc.gayete@students.salle.url.edu>
2023-03-21 12:56:36 +0100

    NavBar done

--- NewSocialGift/src/views/ProfileView.vue
+++ NewSocialGift/src/views/ProfileView.vue
@@ -26,29 +26,44 @@
       <div class="navigation">
         <ul>
           <li>
-            <a href=""> <img src="../../img/logo.png" id="logoNavBar" alt="" /></a>
+            <a href="main"> <img src="../../img/logo.png" id="logoNavBar" alt="" /></a>
           </li>
           <li>
-            <a href=""><i class="fa-solid fa-house"></i> Home</a>
+            <a href="main"><i class="fa-solid fa-house"></i> Home</a>
           </li>
           <li>
-            <a href=""><i class="fa-solid fa-magnifying-glass"></i> Search</a>
+            <a href="#"><i class="fa-solid fa-magnifying-glass"></i> Search</a>
+            <ul id="SearchSlide">
+              <li>
+                <input type="text" placeholder="Search..."/>
+                <i class="fa-solid fa-user"><a href="#">Martin50</a></i>
+                
+              </li>
+            </ul>
           </li>
           <li>
-            <a href=""><i class="fa-solid fa-envelope"></i> Messages</a>
+            <a href="chat"><i class="fa-solid fa-envelope"></i> Messages</a>
           </li>
           <li>
-            <a href=""><i class="fa-sharp fa-solid fa-bell"></i> Notifications</a>
+            <a href="#"><i class="fa-sharp fa-solid fa-bell"></i> Notifications</a>
+            <ul id="NotificationsSlide">
+              <li>Más recientes</li>
+              <li id="InsideNotificationSlide">
+                <i class="fa-solid fa-user"></i>
+                <a>Patricia96</a>
+                <p>Te ha empezado a seguir</p>
+                <button>Seguir</button>
+              </li>
+            </ul>
           </li>
           <li>
             <a href=""><i class="fa-solid fa-pen-to-square"></i> Create</a>
           </li>
           <li id="MoreLi">
             <a href="#"><i class="fa-solid fa-bars"></i> More</a>
-            <ul>
-              <li><a href="#">Mas 1</a></li>
-              <li><a href="#">Servicio 2</a></li>
-              <li><a href="#">Servicio 3</a></li>
+            <ul id="moreSlide">
+              <li><a href="editarperfil">Editar Perfil</a></li>
+              <li><a href="/">Desconectar</a></li>
             </ul>
           </li>
         </ul>
