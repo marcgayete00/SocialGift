ee5884d39fd448132f776b1b3f2b1107bc927d93
marcgayete <marc.gayete@students.salle.url.edu>
2023-04-20 10:16:38 +0200

    Starting login in VUE

--- NewSocialGift/src/views/HomeView.vue
+++ NewSocialGift/src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <script setup>
 import language from './../components/language.vue'
+
+//Obtener texto del input username
+var username = document.getElementById('username')
+var password = document.getElementById('password')
+
+//Obtener el boton de login
+var loginButton = document.getElementById('loginButton')
 </script>
 
 <template>
@@ -10,15 +17,17 @@ import language from './../components/language.vue'
         <div>
           <img src="img/logo.png" id="LogoStyle" />
           <form>
-            <input type="text" class="input" name="username" placeholder="Usuario" /><br/>
+            <input type="text" class="input" name="username" placeholder="Usuario" /><br />
             <input
               type="password"
               class="input"
               id="password"
               name="password"
-              placeholder="Contraseña"/><br/>
+              placeholder="Contraseña"
+            /><br />
             <button type="submit" id="loginButton"><a href="main"> Login </a></button>
-            <hr /><!-- USAR ALTERNATIVA -->
+            <hr />
+            <!-- USAR ALTERNATIVA -->
             <span id="textRegister">¿No tienes cuenta? <a href="register"> Registrate</a></span>
           </form>
         </div>
@@ -40,45 +49,39 @@ import language from './../components/language.vue'
       </div>
     </footer>
   </div>
-  
-  
 </template>
 
 <style>
-    @media screen and (max-width: 600px) {
-      #GeneralSection {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-      }
-      #LoginSection {
-        margin-top: 20px;
-        width: auto;
-        padding-right: 30px !important;
-      }
-      #ImageSection {
-        display: none;
-      }
-      #loginFooter {
-        display: none;
-      }
+@media screen and (max-width: 600px) {
+  #GeneralSection {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  #LoginSection {
+    margin-top: 20px;
+    width: auto;
+    padding-right: 30px !important;
+  }
+  #ImageSection {
+    display: none;
+  }
+  #loginFooter {
+    display: none;
+  }
 
-      #textRegister{
-        display: flex;
-        justify-content: center;
-      }
+  #textRegister {
+    display: flex;
+    justify-content: center;
+  }
 
-      #LogoStyle {
-        display: flex;
-        justify-content: center;
-        margin-left: 60px !important;
-      }
-
-    
-
-
-    }
-  </style>
+  #LogoStyle {
+    display: flex;
+    justify-content: center;
+    margin-left: 60px !important;
+  }
+}
+</style>
 
 <style scoped>
 @import '../assets/HomeStyle.css';
